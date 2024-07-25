0524219eb70d1aeb3b845196434042a60798da1d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-26 00:47:25 +0200

    style: delete logs

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -3,7 +3,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 export const storeData = async (key: string, value: string) => {
   try {
     await AsyncStorage.setItem(key, value)
-    console.log('Data successfully saved')
   } catch (e) {
     console.error('Failed to save the data to the storage', e)
   }
@@ -13,7 +12,6 @@ export const getData = async (key: string) => {
   try {
     const value = await AsyncStorage.getItem(key)
     if (value !== null) {
-      console.log('Data retrieved successfully:', value)
       return value
     }
   } catch (e) {

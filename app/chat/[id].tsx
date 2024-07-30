df9c6c1d19ff818d0f356ede763e5ac7d88c374a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-30 21:25:34 +0200

    build: socket room emition working ok

--- app/chat/[id].tsx
+++ app/chat/[id].tsx
@@ -1,11 +1,59 @@
 import { useRouter, useLocalSearchParams } from 'expo-router'
 import { View, Text, Pressable } from 'react-native'
-import { styles } from '../../utils/constants'
+import { API, styles } from '../../utils/constants'
 import AntDesign from '@expo/vector-icons/AntDesign'
+import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+import { fetchUsername } from '../../utils/lib'
 
 export default function DetailsScreen() {
+  const [currentUserID, setCurrentUserID] = useState<number | undefined>(
+    undefined
+  )
+  const [messages, setMessages] = useState<any[]>([])
+  const [otherFullname, setOtherFullname] = useState<any>()
+  const [socket, setSocket] = useState<any>(null)
+  const [saving, setSaving] = useState<boolean>()
   const { id } = useLocalSearchParams()
   const router = useRouter()
+
+  useEffect((): any => {
+    const newSocket = io('wss://talk-backend-l15w.onrender.com')
+    setSocket(newSocket)
+
+    fetchUsername().then((response) => setCurrentUserID(response.user_id))
+
+    return () => newSocket.close()
+  }, [])
+
+  useEffect(() => {
+    if (id) {
+      fetch(`${API}/api/chats/conversation/${id}`)
+        .then((res) => res.json())
+        .then((response) => setMessages(response))
+    }
+  }, [id])
+
+  useEffect(() => {
+    if (id && currentUserID) {
+      fetch(`${API}/api/users/getOtherUsername`)
+        .then((res) => res.json())
+        .then((response) => setOtherFullname(response.other_fullname))
+    }
+  }, [id, currentUserID])
+
+  useEffect(() => {
+    if (socket && id) {
+      socket.emit('joinRoom', id.toString()) // join to the room with that id
+
+      socket.on('chat message', (message: any) => {
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { content: message.message, sender_id: message.currentUserID },
+        ])
+      })
+    }
+  }, [socket, id])
 
   return (
     <View style={styles.container}>
@@ -13,7 +61,7 @@ export default function DetailsScreen() {
         <View style={styles.header}>
           <View style={styles.chatHeader}>
             <Pressable onPress={() => router.back()}>
-              <AntDesign name='arrowleft' size={24} color='white' />
+              <AntDesign name='arrowleft' size={20} color='white' />
             </Pressable>
             <Text style={styles.text}>Details of chat: {id} </Text>
           </View>

45175b15c51673bf478e0e17736f63a47f5ba20c
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-30 20:47:07 +0200

    build: header ui

--- app/chat/[id].tsx
+++ app/chat/[id].tsx
@@ -1,13 +1,24 @@
-import { useLocalSearchParams } from 'expo-router'
-import { View, Text } from 'react-native'
+import { useRouter, useLocalSearchParams } from 'expo-router'
+import { View, Text, Pressable } from 'react-native'
 import { styles } from '../../utils/constants'
+import AntDesign from '@expo/vector-icons/AntDesign'
 
 export default function DetailsScreen() {
   const { id } = useLocalSearchParams()
+  const router = useRouter()
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Details of chat: {id} </Text>
+      <View style={styles.navContainer}>
+        <View style={styles.header}>
+          <View style={styles.chatHeader}>
+            <Pressable onPress={() => router.back()}>
+              <AntDesign name='arrowleft' size={24} color='white' />
+            </Pressable>
+            <Text style={styles.text}>Details of chat: {id} </Text>
+          </View>
+        </View>
+      </View>
     </View>
   )
 }

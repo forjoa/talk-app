495a0cf96267f1e7260da15b2c5d6d18cb7a7bae
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-08-02 18:08:50 +0200

    build: create my profile page

---
+++ app/myProfile.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import { styles } from '../utils/constants'
+
+export default function MyProfile() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My profile</Text>
+    </View>
+  )
+}
